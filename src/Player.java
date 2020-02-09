74f75f3a2cb0d56edb6e78ee53a58298306879a3
JenniferFlanagan <flanagan.jennifera@gmail.com>
2020-02-09 21:51:52 +0000

    Here is the Player class, it contains the following functions:
    - Data reset function: it allows the players data to be reset once needed
    - Player name function: it allows the players name to be set in the game
    - Display score function: it allows the player to have access to their score
    - Get score: it gives the ability to get the score
    - Display name function: it allows the name of the player to be displayed
    
    NOTE: This does not contain the Increase Score function and the frame function as this has to be added at a later time as to not accidentally alter code. It will be added later

---
+++ src/Player.java
@@ -0,0 +1,48 @@
+
+public class Player {
+
+    //Instance variables
+    protected String player_name;
+    protected int player_score;
+
+
+
+
+    //A1 - Allows the player data to be reset
+    public void data_reset()
+    {
+        player_name = null;
+        player_score = 0;
+    }
+
+    public Player(String name)
+    {
+        this.player_name = name;
+        player_score = 0;
+    }
+
+    //A2 - Allows the name of the player to be set
+    public void setName(String newName) {
+        this.player_name = newName;
+    }
+    public String getName() {
+        return player_name;
+    }
+
+
+
+    //A4 - Allows access to their score
+    public void display_score()
+    {
+        System.out.println(this.player_name + "'s score is: " + this.player_score);
+    }
+
+    //Get score
+    public int getScore() {return player_score;}
+
+    //A6 - Allows display of a players name
+    public void display_name()
+    {
+        System.out.println(player_name);
+    }
+}
