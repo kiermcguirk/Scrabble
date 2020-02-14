f462d39ab24e2ce237799214a80d3a140afff5bc
JenniferFlanagan <flanagan.jennifera@gmail.com>
2020-02-14 10:32:29 +0000

    Board reset

--- src/Board.java
+++ src/Board.java
@@ -1,9 +1,32 @@
 public class Board {
 
-    public static Square[] [] game_board = new Square[15][15];
+    public static Square[][] game_board = new Square[15][15];
 
-    public Board()
-    {
-        System.out.println("Jennifer push");
+    public Board() {
+
+    }
+
+    public void board_reset() {
+        for (int i = 0; i <= 15; i++) {
+            for (int j = 0; j <= 15; j++) {
+
+                if (game_board[i][j].tile == null) {
+                    continue;
+                } else {
+                    game_board[i][j].tile = null;
+                }
+            }
+        }
+    }
+
+
+    public void display_board() {
+        for (int i = 0; i <= 15; i++) {
+            for (int j = 0; j <= 15; j++) {
+
+
+            }
+        }
+
     }
 }
