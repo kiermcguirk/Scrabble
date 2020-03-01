7a2f06c251292debeec31c4d37e068389812d2c7
kiermcguirk <kier.mcguirk@ucdconnect.ie>
2020-03-01 21:22:30 +0000

    --Includes-- 1. First glance at the board

--- src/main.java
+++ src/main.java
@@ -1,16 +1,34 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+
+import java.awt.*;
 
 public class main extends Application {
-
+    Stage board;
+    Button start;
 
     public static void main(String[]args)
     {
-        System.out.println("x");
+        Launch(args);
     }
 
     @Override
     public void start(Stage stage) throws Exception {
 
+        board = stage;
+        board.setTitle("Scrabble Board");
+        start = new Button("Click here to start game");
+
+        BorderPane game_layout = new BorderPane();
+        game_layout.getChildren().add(start);
+
+        Scene scene = new Scene(game_layout,200,200);
+
+
+        board.show();
     }
 }
