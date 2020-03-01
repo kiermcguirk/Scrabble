8d6da3a8a91022e3dda998884def33022d23be1e
kiermcguirk <kier.mcguirk@ucdconnect.ie>
2020-03-01 21:30:48 +0000

    --Includes-- 1. Start Button

--- src/main.java
+++ src/main.java
@@ -1,34 +1,38 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
 
 import java.awt.*;
+import java.util.function.BinaryOperator;
 
 public class main extends Application {
     Stage board;
-    Button start;
+    Button startButton;
 
-    public static void main(String[]args)
+    public static void main(String[] args)
     {
-        Launch(args);
+        launch(args);
     }
+
+
 
     @Override
     public void start(Stage stage) throws Exception {
 
         board = stage;
         board.setTitle("Scrabble Board");
-        start = new Button("Click here to start game");
-
-        BorderPane game_layout = new BorderPane();
-        game_layout.getChildren().add(start);
+        startButton = new Button("Click here to start game");
+        startButton.setBorder(new Border(new BorderStroke(Color.GREEN, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+        StackPane game_layout = new StackPane();
+        game_layout.getChildren().add(startButton);
 
         Scene scene = new Scene(game_layout,200,200);
 
-
+        board.setScene(scene);
         board.show();
     }
 }
