ee529226555a92a41489937c126f3825daf4efe4
Aman Parary <aman.parary@ucdconnect.ie>
2020-03-06 12:13:34 +0000

    Home Page

---
+++ src/Controller.java
@@ -0,0 +1,57 @@
+import javafx.fxml.FXML;
+
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.event.ActionEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private Label label;
+
+    @FXML
+    public Button startGame;
+    public Button backButton;
+    public Button quitButton;
+
+    @FXML
+    public void getAction(ActionEvent event) throws IOException {
+        Parent gameMode = FXMLLoader.load(getClass().getResource("getContents.fxml"));
+        Scene gameScene = new Scene(gameMode);
+
+        //Get stage information
+        Stage window = (Stage) startGame.getScene().getWindow();
+        window.setScene(gameScene);
+        window.show();
+    }
+
+    @FXML
+    public void backEvent(ActionEvent event) throws IOException{
+        Parent back = FXMLLoader.load(getClass().getResource("homePage.fxml"));
+        Scene backScene = new Scene(back);
+
+        Stage backWindow = (Stage) backButton.getScene().getWindow();
+        backWindow.setScene(backScene);
+        backWindow.show();
+    }
+
+    @FXML
+    public void quitEvent(ActionEvent event) throws IOException{
+        Stage stage = (Stage) quitButton.getScene().getWindow();
+        stage.close();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
--- src/main.java
+++ src/main.java
@@ -1,38 +1,22 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
-import javafx.scene.control.Label;
-import javafx.scene.control.Button;
-
-import java.awt.*;
-import java.util.function.BinaryOperator;
-
 public class main extends Application {
-    Stage board;
-    Button startButton;
 
     public static void main(String[] args)
     {
         launch(args);
     }
 
-
-
     @Override
     public void start(Stage stage) throws Exception {
-
-        board = stage;
-        board.setTitle("Scrabble Board");
-        startButton = new Button("Click here to start game");
-        startButton.setBorder(new Border(new BorderStroke(Color.GREEN, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
-        StackPane game_layout = new StackPane();
-        game_layout.getChildren().add(startButton);
-
-        Scene scene = new Scene(game_layout,200,200);
-
-        board.setScene(scene);
-        board.show();
+        Parent root = FXMLLoader.load(getClass().getResource("homePage.fxml"));
+        stage.getIcons().add(new Image("/Images/image.jpg")); //create icon
+        stage.setTitle(" Scrabble Board");
+        stage.setScene(new Scene(root, 600, 400));
+        stage.show();
     }
 }
