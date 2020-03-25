f13b3bdcdef12cfd2bf047bff79b729dcc05e2ca
Aman Parary <aman.parary@ucdconnect.ie>
2020-03-25 15:54:29 +0000

    -Added Action-event for Play game button -Fixed Username text on labels

--- src/main.java
+++ src/main.java
@@ -2,38 +2,18 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-import java.awt.*;
-import java.util.ArrayList;
-
 public class main extends Application {
-    private static int X_SQUARE_NUM = 15;
-    private static int Y_SQUARE_NUM = 15;
-    //public Board board = new Board();
-    //public layoutManager lm = new layoutManager();
-    //public static UI ui = new UI();
-
 
     @Override
     public void start(Stage stage) throws Exception {
-
-        //stage = ui.lm.getStage();
-        //ui.lm.promptUserBeginGame();
 
         Parent root = FXMLLoader.load(getClass().getResource("homePage.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.setTitle(" Scrabble Board");
         stage.show();
-
-
     }
 
     public static void main(String[] args)
